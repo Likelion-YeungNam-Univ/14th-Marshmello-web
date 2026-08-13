896a39b5268fa589c13218a6025f97cf1e185f20
Park jin-a <jiiinap@naver.com>
2026-08-13 21:09:34 +0900

    fix: 콘솔 창 로그인 팝업 경고 메시지 해결(vite.config.ts 내 COOP 헤더 설정)

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     open: true,
+    // 구글 로그인 팝업 창 차단 경고 해결을 위한 헤더 설정 추가
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
   },
   resolve: {
     alias: {
