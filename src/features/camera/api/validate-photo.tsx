00726833069c27bae6d8deeb39b43863ec3a2122
BOOK-F1C2CH8AJQ\jhl45 <wjdgudwn36@gmail.com>
2026-08-19 04:18:03 +0900

    refactor: Blob변환이 아닌 File타입 전송 함수로 변경

--- src/features/camera/api/validate-photo.tsx
+++ src/features/camera/api/validate-photo.tsx
@@ -1,6 +1,7 @@
 //이미지 컨트롤러의 api를 가져옴
 import {analyzeCheckInImage, type analyzeImageResponse,} from "@/features/test/checkin-Image-Controller"
 
+//타당한지 아닌지 검증하는 api
 export function validatePhoto(image: File,): Promise<analyzeImageResponse> {
   return analyzeCheckInImage({image,})
 }
