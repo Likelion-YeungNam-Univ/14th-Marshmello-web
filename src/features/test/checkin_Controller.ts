9eb591923e33bac87dda054e8876f6960fb25ef8
Parkjina <jiiinap@naver.com>
2026-08-19 12:05:29 +0900

    fix: checkin controller 충돌 해결

--- src/features/test/checkin_Controller.ts
+++ src/features/test/checkin_Controller.ts
@@ -119,7 +119,6 @@ export async function createCheckIn(
           date,
         },
       },
-      },  
     )
 
   return response.data
@@ -141,7 +140,7 @@ export async function getCheckInEmotions(
 //체크인 한 횟수
 export async function getcheckInCount(
   month: string,
-) : Promise<checkInCountResponse> {
+): Promise<checkInCountResponse> {
   const url =
     `/api/check-ins/count?month=${month}`
 
@@ -154,7 +153,7 @@ export async function getcheckInCount(
 //그 달에 가장 많이 불편함을 호소한 부위
 export async function getcheckInRegion(
   month: string,
-) : Promise<checkInRegion> {
+): Promise<checkInRegion> {
   const url =
     `/api/check-ins/body-diaries/top-region?month=${month}`
 
@@ -182,7 +181,10 @@ export const handleGetCheckInsByDate =
 
 // POST /api/check-ins 테스트
 export const handleCreateCheckIn =
-  async (date: string, request: checkInCreateRequest) => {
+  async (
+    date: string,
+    request: checkInCreateRequest,
+  ) => {
     const result =
       await createCheckIn(date, request)
 
